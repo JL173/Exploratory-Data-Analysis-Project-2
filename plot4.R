88756792d1c88f917d828ad69403c0553f9d7a15
JL <89531432+JL173@users.noreply.github.com>
2021-09-01 12:06:43 +0100

    plot4

---
+++ plot4.R
@@ -0,0 +1,35 @@
+plot4 <- function(){
+  
+  library(tidyverse)
+  
+  NEI <- readRDS("summarySCC_PM25.rds")
+  
+  # NEI year stored as int. We'll set to factors
+  NEI$year <- as.factor(NEI$year)
+  
+  # Plot4.R
+  
+  # Need all "Combustion" "Coal" SCC codes from SCC
+  # to cross-reference NEI. That is if Short.Name
+  # contains BOTH "Comb" and "Coal"
+  
+  coal_scc <- SCC %>%
+    filter(grepl("Comb.*Coal", Short.Name))%>%
+    select(SCC)
+  
+  us_coal <- NEI %>%
+    filter(SCC %in% coal_scc$SCC) %>%
+    group_by(year) %>%
+    summarise(Total = sum(Emissions, na.rm=TRUE)/1e3)
+  
+  
+  # png device for plotting
+  png(filename = "plot4.png", width = 480, height = 480)
+  
+  ggplot(us_coal, aes(x = year, y = Total)) + 
+    geom_col(position = "dodge", colour = "black") +
+    ylab("Total (thousand tons)") + xlab(NULL) +
+    ggtitle("Total PM2.5 Emissions in United States\nfrom Coal combustion-related sources")
+  
+  dev.off() # close device
+}
